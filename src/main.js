6c65fdee3eb42e7dad1b3bc2464e2870c8746d31
mugen <405339683@qq.com>
2019-05-06 19:30:33 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+// lib有.css文件
+// 引入文件也没问题
+// 该文件无法处理->让webpack
+
+// 引入MUI相关的资源
+import './lib/mui/css/mui.min.css'
+
+import './lib/mui/css/icons-extra.css'
+
+Vue.use(MintUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
