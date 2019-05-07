e691731d42f77f2c81ba96b8b08de2abf3151519
mugen <405339683@qq.com>
2019-05-07 13:21:27 +0800

    home done

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005'
+Vue.prototype.$http = axios
 
 // lib有.css文件
 // 引入文件也没问题
